f528ff8e9eef8fdd741e8b2c025b69fe0f448d0e
Ayush Dixit <critikal298@gmail.com>
2026-07-12 10:19:23 +0530

    Update main.py

--- port-scanner/main.py
+++ port-scanner/main.py
@@ -1,15 +1,21 @@
 import socket
 ip = input("enter ipadress ")
-port = int(input("port "))
-s=socket.socket()
+port = int(input("enter start "))
+porto = int(input("enter end "))
 
-s.settimeout(3)
-try:
-    s.connect((ip,port))
-    print("port is open ")
+while(port<=porto):
+    s=socket.socket()
 
-except TimeoutError:
-        print("TIme out")
-except ConnectionRefusedError:
-      print("port is closedd")
-s.close() 
+    s.settimeout(3)
+    try:
+        s.connect((ip,port))
+        print(f"[+] Port {port} OPEN")
+
+    except TimeoutError:
+        print(f"[+] Port {port} TIMEOUT")
+    except ConnectionRefusedError:
+        print(f"[+] Port {port} CLOSE")
+    except OSError as e:
+        print(e)
+    port=port+1
+    s.close() 
